297ef7a5c9b6f94431abf4dd83bf135803825404
edupoly <info.edupoly@gmail.com>
2026-04-16 14:02:00 +0530

    templating-pug

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ app.get("/home/:fullname", (req, res) => {
   res.render("index", { fullname: req.params.fullname });
 });
 
-var si = 0;
-var ei = 20;
-app.get("/movies", (req, res) => {
+app.get("/movies{/:si}{/:perpage}", (req, res) => {
+  var startIndex = +req.params.si || 0;
+  var perPage = +req.params.perpage || 10;
+
+  console.log(req.params.perpage);
   var fd = fs.readFileSync("movies.json").toString();
-  var movies = JSON.parse(fd).slice(si, ei);
-  res.render("movies", { movies });
+  var movies = JSON.parse(fd).slice(startIndex, startIndex + perPage);
+  res.render("movies", { movies, startIndex, perPage });
   // res.send(movies);
 });
 
