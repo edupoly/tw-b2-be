724e40831a898b7d0e6a70cb090ed80ed699cea4
edupoly <info.edupoly@gmail.com>
2026-04-16 14:07:12 +0530

    apis

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 var express = require("express");
 var app = express();
 var fs = require("fs");
-app.set("view engine", "pug");
 
-app.get("/home/:fullname", (req, res) => {
-  res.render("index", { fullname: req.params.fullname });
+app.get("/movies", (req, res) => {
+  var fd = fs.readFileSync("movies.json").toString();
+  var movies = JSON.parse(fd);
+  res.json(movies);
 });
 
-app.get("/movies{/:si}{/:perpage}", (req, res) => {
-  var startIndex = +req.params.si || 0;
-  var perPage = +req.params.perpage || 10;
-
-  console.log(req.params.perpage);
-  var fd = fs.readFileSync("movies.json").toString();
-  var movies = JSON.parse(fd).slice(startIndex, startIndex + perPage);
-  res.render("movies", { movies, startIndex, perPage });
-  // res.send(movies);
+app.get("/issues", (req, res) => {
+  var fd = fs.readFileSync("issues.txt").toString();
+  var issues = JSON.parse(fd);
+  res.json(issues);
 });
 
 app.listen(3600, () => {
