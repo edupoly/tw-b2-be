c50d362fedbf215e6bf11584a440195c98ba1508
edupoly <info.edupoly@gmail.com>
2026-04-24 14:24:26 +0530

    mongodb-connectivity

--- server.js
+++ server.js
@@ -1,90 +1,46 @@
+require("dotenv").config();
+
 var express = require("express");
 var app = express();
-var fs = require("fs");
-var cors = require("cors");
+var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
-var jwt = require("jsonwebtoken");
-var { v4: uuid } = require("uuid");
+var TodoModel = require("./model/todo.model");
+var LeadModel = require("./model/lead.model");
+var connectDB = require("./db");
 
-app.use(cors());
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post("/login", (req, res) => {
-  console.log(req.body);
-  var fd = JSON.parse(fs.readFileSync(__dirname + "/users.txt").toString());
-  var k = fd.find((user) => {
-    if (
-      user.username === req.body.username &&
-      user.password === req.body.password
-    ) {
-      return true;
-    }
+connectDB();
+
+app.post("/addtodo", (req, res) => {
+  var newTodo = new TodoModel({
+    title: req.body.ntd,
+    status: true,
+    timeStamp: Date.now(),
   });
-  if (k) {
-    //gen token
-    var token = jwt.sign({ ...req.body }, "neekendu");
-    res.send({ msg: "loginsuccess", token, username: req.body.username });
-  } else {
-    res.send({ msg: "loginfailed" });
-  }
+  newTodo.save();
 });
 
 app.get("/todos", (req, res) => {
-  console.log(req.headers.token);
-  var k = jwt.verify(req.headers.token, "neekendu");
-  console.log("token details");
-  console.log(k);
-  var fd = JSON.parse(fs.readFileSync(__dirname + "/todos.txt").toString());
-  var userTodos = fd.filter((todo) => {
-    if (todo.username === k.username) {
-      return true;
-    }
+  TodoModel.find().then((data) => {
+    res.send(data);
   });
-  res.send(userTodos);
 });
 
-app.post("/todos", (req, res) => {
-  console.log(req.headers.token);
-  var k = jwt.verify(req.headers.token, "neekendu");
-  console.log("token details");
-  console.log(k.username);
-  var fd = JSON.parse(fs.readFileSync(__dirname + "/todos.txt").toString());
-  fd.push({
-    title: req.body.todo,
-    username: k.username,
-    status: "notcompleted",
-    id: uuid(),
-  });
-  fs.writeFileSync(__dirname + "/todos.txt", JSON.stringify(fd));
-  res.send({ msg: "add ipoindi" });
-});
-
-app.delete("/todos/:id", (req, res) => {
-  var fd = JSON.parse(fs.readFileSync(__dirname + "/todos.txt").toString());
-  fd = fd.filter((todo) => {
-    if (todo.id !== req.params.id) {
-      return true;
-    }
-  });
-  fs.writeFileSync(__dirname + "/todos.txt", JSON.stringify(fd));
-  res.send({ msg: "delete ipoindi" });
-});
-
-app.put("/todos/:id", (req, res) => {
+app.post("/addlead", (req, res) => {
   console.log(req.body);
-  var fd = JSON.parse(fs.readFileSync(__dirname + "/todos.txt").toString());
-  fd = fd.map((todo) => {
-    if (todo.id === req.params.id) {
-      return req.body;
-    } else {
-      return todo;
-    }
+  var newLead = new LeadModel(req.body);
+  newLead.save().then(() => {
+    res.send("Ipoindi");
   });
-  fs.writeFileSync(__dirname + "/todos.txt", JSON.stringify(fd));
-  res.send({ msg: "updated" });
 });
 
-app.listen(3600, () => {
+app.get("/", (req, res) => {
+  res.send("aagara babu");
+});
+
+app.listen(process.env.PORT || 3600, () => {
   console.log("server 3600 port lo vintundi");
 });
